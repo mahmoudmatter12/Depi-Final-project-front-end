ab9ca87ebfcd110a7211db7e96e53242278ae2fc
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-04-18 21:03:01 +0200

    Landing page

---
+++ components/reUsed/btn.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils';
+import React from 'react'
+import { Button } from '../ui/button';
+
+interface BtnProps {
+    onClick?: () => void;
+    children:React.ReactNode
+    className?: string
+    type?: "fill" | "out_line" 
+}
+
+const btn_types = {
+    "out_line": "bg-transparent border-2 border-gray-500 hover:border-cyan-400 px-8 py-3 rounded-lg text-lg font-semibold transition",
+    "fill": "bg-cyan-600 hover:bg-cyan-700 rounded-lg text-lg font-semibold transition"
+}
+
+export default function Btn({ children, onClick, className, type = "fill" }: BtnProps) {
+    return (
+        <Button className={cn(btn_types[type], className)} onClick={onClick}>
+            {children}
+        </Button>
+    )
+}
