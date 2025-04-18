9881764a1ef809d4e27134b39c77b8f023b39ce1
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-04-18 15:27:37 +0200

    getting started

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+function AboutPage() {
+  return (
+    <>
+    <h1>
+        About page
+    </h1>
+    </>
+  )
+}
+
+export default AboutPage;
+
+// localhost:3000/about
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+// 
+// const user1 = {
+//   name: "John Doe",
+//   age: 30,
+//   role: "user-gust",
+// }
 
 function page() {
   return (
