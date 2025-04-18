f63d8e7b114f68816bba17e42bfa0689b160c1dd
mahmoudmatter12 <HNU.24.363.Mahmoud-zEl-Din@hnu.edu.eg>
2025-04-18 22:21:33 +0200

    updated landing footer and header with motions

--- components/reUsed/btn.tsx
+++ components/reUsed/btn.tsx
@@ -6,12 +6,13 @@ interface BtnProps {
     onClick?: () => void;
     children:React.ReactNode
     className?: string
-    type?: "fill" | "out_line" 
+    type?: "fill" | "out_line" | "Danger" 
 }
 
 const btn_types = {
-    "out_line": "bg-transparent border-2 border-gray-500 hover:border-cyan-400 px-8 py-3 rounded-lg text-lg font-semibold transition",
-    "fill": "bg-cyan-600 hover:bg-cyan-700 rounded-lg text-lg font-semibold transition"
+    "out_line": "bg-transparent border-2 border-gray-500 hover:border-cyan-400 px-8 py-3 rounded-lg text-xl font-semibold transition",
+    "fill": "bg-cyan-600 hover:bg-cyan-700 rounded-lg text-xl font-semibold transition",
+    "Danger": "bg-red-600 hover:bg-red-700 text-white rounded-lg text-xl font-semibold transition"
 }
 
 export default function Btn({ children, onClick, className, type = "fill" }: BtnProps) {
